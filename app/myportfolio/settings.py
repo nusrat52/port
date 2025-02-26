9b33bf16b3c6ce7389df404e5a167e3171b2a4ad
nusrat52 <nusretibragimli@gmail.com>
2025-02-27 01:05:54 +0400

    my

--- app/myportfolio/settings.py
+++ app/myportfolio/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'owner'
+    'owner',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -48,9 +49,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'myportfolio.urls'
+CSRF_TRUSTED_ORIGINS = ['https://*.nusretibrahimli.life']
 
  
 TEMPLATES = [
